8771f95842647539d06efccff78aba4638c79c55
Andres <marianoandres191200@hotmail.com>
2025-07-01 20:41:32 -0300

    font-added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
-import Layout from "./layout";
 import MainRouter from "./router/MainRouter";
-import Home from "./screens/Home";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default function App() {
 
