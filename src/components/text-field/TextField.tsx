df6b3855c1e839094f1fc7dfe7782a5f1b34abef
Andres <marianoandres191200@hotmail.com>
2025-07-06 17:31:04 -0300

    typing animation package added

--- src/components/text-field/TextField.tsx
+++ src/components/text-field/TextField.tsx
@@ -1,17 +1,27 @@
-import { TypeAnimation } from 'react-type-animation';
-import '../../declaration.d.ts';
-import styles from './styles.module.scss';  
+import { TypeAnimation } from "react-type-animation";
+import "../../declaration.d.ts";
+import styles from "./styles.module.scss";
 
-var typingSequence:any[] = ['Do the dishes', 1000, 'Take the dog for a walk', 1000, 'Feed the cat', 1000, 'Do the homework']
+var typingSequence: any[] = [
+  "Do the dishes",
+  1000,
+  "Take the dog for a walk",
+  1000,
+  "Feed the cat",
+  1000,
+  "Do the homework",
+  1000
+];
 
 export default function TextField() {
   return (
     <div className={styles.textFieldComponent}>
-        <div className={styles.inputContainer}>
-            <input type="text" placeholder=''/>
-            <span>Do the dishes</span>
-            <TypeAnimation sequence={typingSequence} wrapper='span'/>
+      <div className={styles.inputContainer}>
+        <input type="text" placeholder="" />
+        <div className={styles.placeholderWrapper}>
+          <TypeAnimation sequence={typingSequence} wrapper="span" repeat={Infinity}/>
         </div>
+      </div>
     </div>
   );
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -7,7 +7,7 @@ export default function Home(){
     return(
         <div className={styles.homeScreen}>
             <h1>Welcome to a simple ToDo App!</h1>
-            <MessageBox message='Create your first To Do right away :)'/>
+            <p className={styles.subtitle}>Create your first To Do right away :)</p>
             <TextField />
         </div>
     )
