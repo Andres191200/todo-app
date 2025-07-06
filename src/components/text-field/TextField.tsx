93141d6b85511c66209c2ba773ec206be360a2be
Andres <marianoandres191200@hotmail.com>
2025-07-06 17:21:48 -0300

    testing animation

--- src/components/text-field/TextField.tsx
+++ src/components/text-field/TextField.tsx
@@ -1,5 +1,8 @@
+import { TypeAnimation } from 'react-type-animation';
 import '../../declaration.d.ts';
-import styles from './styles.module.scss';
+import styles from './styles.module.scss';  
+
+var typingSequence:any[] = ['Do the dishes', 1000, 'Take the dog for a walk', 1000, 'Feed the cat', 1000, 'Do the homework']
 
 export default function TextField() {
   return (
@@ -7,6 +10,7 @@ export default function TextField() {
         <div className={styles.inputContainer}>
             <input type="text" placeholder=''/>
             <span>Do the dishes</span>
+            <TypeAnimation sequence={typingSequence} wrapper='span'/>
         </div>
     </div>
   );
