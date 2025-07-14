4fd8172b63e93cb3ae117253f714ea9b0dbb02d1
Andres <marianoandres191200@hotmail.com>
2025-07-13 21:32:03 -0300

    moved  box shadow animation from the input component itself outside

--- src/components/todo-form/TodoForm.tsx
+++ src/components/todo-form/TodoForm.tsx
@@ -16,38 +16,52 @@ var typingSequence: any[] = [
 ] as const;
 
 type TFormFields = {
-  title: string,
-  description: string,
-}
+  title: string;
+  description: string;
+};
 
 export default function TodoForm() {
   const [form, setForm] = useState<TFormFields>({
-    title: '',
-    description: '',
+    title: "",
+    description: "",
   });
 
-  function handleDescriptionChange(event: React.ChangeEvent<HTMLTextAreaElement>){
-    setForm(prevFormState => ({...prevFormState, 'description': event.target.value}) satisfies TFormFields);
-  }
-  
-  function handleTitleChange(event: React.ChangeEvent<HTMLInputElement>) {
-    setForm(prevFormState => ({...prevFormState, 'title': event.target.value}) satisfies TFormFields);
+  function handleDescriptionChange(
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) {
+    setForm(
+      (prevFormState) =>
+        ({
+          ...prevFormState,
+          description: event.target.value,
+        } satisfies TFormFields)
+    );
   }
 
-  function submitTodo(){
-    console.log('title: ', form.title);
-    console.log('description: ', form.description);
+  function handleTitleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setForm(
+      (prevFormState) =>
+        ({ ...prevFormState, title: event.target.value } satisfies TFormFields)
+    );
+  }
+
+  function submitTodo() {
+    console.log("title: ", form.title);
+    console.log("description: ", form.description);
   }
 
   return (
     <div className={styles.todoFormComponent}>
-      <TextField
-        placeholderSequence={typingSequence}
-        onChange={(event) => handleTitleChange(event)}
-      />
+      <div className={styles.textFieldContainer}>
+        <TextField
+          placeholderSequence={typingSequence}
+          onChange={(event) => handleTitleChange(event)}
+        />
+      </div>
+
       {form.title ? (
         <div className="fadeInFromTop">
-          <TextArea onChange={(event) => handleDescriptionChange(event)}/>
+          <TextArea onChange={(event) => handleDescriptionChange(event)} />
         </div>
       ) : null}
       {form.title ? (
